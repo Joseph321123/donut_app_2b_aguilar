ccf45659e3e55ce100afc4690707c3ca46e68cf2
Joseph321123 <aguilarrodriguez1.15@gmail.com>
2025-03-19 11:19:18 -0600

    UI Finished/Adding new products

--- lib/tabs/pancakes_tab.dart
+++ lib/tabs/pancakes_tab.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
-
+import 'package:donut_app_2b_aguilar/utils/donut_tile.dart';
 class PancakesTab extends StatelessWidget {
-  const PancakesTab({super.key});
+  final List donutsOnsale =[
+    //[donutFlavor, donutStore, donutPrice, donotColor,imageName]
+    ["Ice Cream","Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry","Dunkin Donuts", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape","Costco", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco","Walmart", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    ["Ice Cream","Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry","Dunkin Donuts", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape","Costco", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco","Walmart", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    
+  ];
+  PancakesTab({super.key});
+
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("PancakesTab"),
+  
+    //Para acomodar ellementos en cuadricula //GridView
+    return  GridView.builder(
+      // cuantos elementos tengo
+      // elementos en nuestra lista //itemCount
+      itemCount: donutsOnsale.length,
+      padding: const EdgeInsets.all(8.0),
+      //prepa 1: organiza como distribuir elementos en la cuadricula //gridDelegate
+      // sliver permite crear listas y scrolls eficientes con efectos personalizados y optimizados //sliver
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //numero de columnas //crossAxisCount
+        crossAxisCount: 2,
+        //relacion de aspecto (proporcion)
+        childAspectRatio: 1/1.3
+        ),
+      itemBuilder: (context, index){
+        return DonutTile(
+          donutFlavor: donutsOnsale[index][0],
+          donutStore: donutsOnsale [index][1],
+          donutPrice: donutsOnsale[index][2],
+          donutColor: donutsOnsale[index][3],
+          imageName: donutsOnsale[index][4],
+
+        );
+      },
     );
   }
 }
